bf8c33733f6135bb6cf55f54a181843900893226
GiorgiZabakhidze <zabaxidzegivi@gmail.com>
2026-06-10 13:13:34 +0400

    added GoogleCalendarConfig

---
+++ server/src/main/java/ge/freeroom/freeroom/config/GoogleCalendarConfig.java
@@ -0,0 +1,38 @@
+package ge.freeroom.freeroom.config;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+@Configuration
+public class GoogleCalendarConfig {
+    @Value("${google.calendar.credentials.path}")
+    private String credentialsPath;
+
+    @Bean
+    public Calendar calendarClient() throws IOException, GeneralSecurityException {
+        GoogleCredentials credentials = GoogleCredentials
+                .fromStream(new ClassPathResource(credentialsPath).getInputStream())
+                .createScoped(Collections.singleton(CalendarScopes.CALENDAR_READONLY));
+
+        HttpRequestInitializer reqInitializer = new HttpCredentialsAdapter(credentials);
+
+        return new Calendar.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+                GsonFactory.getDefaultInstance(),
+                reqInitializer)
+                .setApplicationName("FreeRoom")
+                .build();
+    }
+}
