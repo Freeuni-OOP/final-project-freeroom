db92a9b4111b6b02d4701bdd4b959e7dc471fb41
llukito <lukaaladashvili21@gmail.com>
2026-06-12 13:36:35 +0400

    style: match 2-space indentation and add trailing semicolon in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '@/context';
-
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -12,4 +11,4 @@ createRoot(document.getElementById('root')).render(
             <App />
         </AuthProvider>
     </BrowserRouter>
-)
+);
