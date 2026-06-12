e26fe73998c5e8490a74b75088905518f8d0a2d8
llukito <lukaaladashvili21@gmail.com>
2026-06-12 15:48:56 +0400

    feat(auth): protect /profile and /floors routes with ProtectedRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
 import { LandingPage, ProfilePage, FloorsPage } from '@/pages';
+import { ProtectedRoute } from '@/components';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/floors" element={<FloorsPage />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route
+                path="/profile"
+                element={
+                    <ProtectedRoute>
+                        <ProfilePage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/floors"
+                element={
+                    <ProtectedRoute>
+                        <FloorsPage />
+                    </ProtectedRoute>
+                }
+            />
+        </Routes>
+    );
 }
 
 export default App;
