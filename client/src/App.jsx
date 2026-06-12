3fd550fb5c71b0b56205161787685348829d0a3b
llukito <lukaaladashvili21@gmail.com>
2026-06-12 23:12:00 +0400

    feat(routing): add catch-all route for unknown URLs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { LandingPage, ProfilePage, FloorsPage } from '@/pages';
+import { LandingPage, ProfilePage, FloorsPage, NotFoundPage } from '@/pages';
 import { ProtectedRoute } from '@/components';
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
                 <Route path="/profile" element={<ProfilePage />} />
                 <Route path="/floors" element={<FloorsPage />} />
             </Route>
+            <Route path="*" element={<NotFoundPage />} />
         </Routes>
     );
 }
