6d4c5d843d961966609cca1626efb18bc6e117ee
llukito <lukaaladashvili21@gmail.com>
2026-06-12 13:20:15 +0400

    feat(auth): wrap app with AuthProvider for global auth state

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from '@/context';
 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </BrowserRouter>
 )
