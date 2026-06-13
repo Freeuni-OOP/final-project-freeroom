f5977718321dae94405e30669ef31f63094376e9
Nick <nikdanelia@gmail.com>
2026-06-14 03:13:02 +0400

    chore: fix import

--- server/src/test/java/ge/freeroom/freeroom/FreeroomApplicationTests.java
+++ server/src/test/java/ge/freeroom/freeroom/FreeroomApplicationTests.java
@@ -3,14 +3,14 @@ package ge.freeroom.freeroom;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import com.google.api.services.calendar.Calendar;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class FreeroomApplicationTests {
 
-	@MockBean
+	@MockitoBean
 	private Calendar calendarClient;
 
 	@Test
