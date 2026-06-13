bca550fcaf7c25e22d736ccee92d190156625d52
Nick <nikdanelia@gmail.com>
2026-06-14 03:09:54 +0400

    test: fix CI pipeline crash by mocking Google Calendar bean

--- server/src/test/java/ge/freeroom/freeroom/FreeroomApplicationTests.java
+++ server/src/test/java/ge/freeroom/freeroom/FreeroomApplicationTests.java
@@ -3,10 +3,15 @@ package ge.freeroom.freeroom;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.google.api.services.calendar.Calendar;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class FreeroomApplicationTests {
+
+	@MockBean
+	private Calendar calendarClient;
 
 	@Test
 	void contextLoads() {
