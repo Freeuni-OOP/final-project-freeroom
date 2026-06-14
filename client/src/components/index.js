90fb6e2fc820a51eb3a7c13b6b608aa41f97b40c
llukito <lukaaladashvili21@gmail.com>
2026-06-14 17:57:18 +0400

    feat(layout): add Layout with navbar and outlet for protected pages

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export { default as FloorView } from './FloorView';
 export { default as RoomModal } from './RoomModal';
 export { default as ProtectedRoute } from './ProtectedRoute';
 export { default as Navbar } from './Navbar';
+export { default as Layout } from './Layout';
