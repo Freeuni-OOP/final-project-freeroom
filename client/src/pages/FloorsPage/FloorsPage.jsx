726613eabc66871ef12e076b8012cc0bb2d3856c
Nick <nikdanelia@gmail.com>
2026-06-14 20:23:00 +0400

    style: make map take up part of the screen

--- client/src/pages/FloorsPage/FloorsPage.jsx
+++ client/src/pages/FloorsPage/FloorsPage.jsx
@@ -5,8 +5,10 @@ export default function FloorsPage() {
   useFloorsPage();
 
   return (
-    <div className="flex flex-col h-screen">
-      <FloorView />
+    <div className="w-full max-w-6xl mx-auto px-4 py-6 md:py-10">
+      <div className="h-[500px] md:h-[700px]">
+        <FloorView />
+      </div>
     </div>
   );
 }
