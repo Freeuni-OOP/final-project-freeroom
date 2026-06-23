d39c0f0ad559c89b71e8f4ad6cc0be07d339fe15
chap0 <nikolozchapidze4@gmail.com>
2026-06-23 22:29:38 +0400

    fix(occupancies): added a query for active occupancies to RoomOccupancyRepository

--- server/src/main/java/ge/freeroom/freeroom/repositories/RoomOccupancyRepository.java
+++ server/src/main/java/ge/freeroom/freeroom/repositories/RoomOccupancyRepository.java
@@ -4,7 +4,10 @@ import ge.freeroom.freeroom.entities.RoomOccupancy;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +16,9 @@ public interface RoomOccupancyRepository extends JpaRepository<RoomOccupancy, Lo
     Optional<RoomOccupancy> findFirstByRoomIdAndEndAtIsNull(Long roomId);
     List<RoomOccupancy> findByRoomIdOrderByCreatedAtDesc(Long roomId);
     List<RoomOccupancy> findByRoomIdInAndEndAtIsNull(List<Long> roomIds);
+
+    // added this query to replace the last one(will leave that just in case)
+    // this one also checks if expected time is not expired, so we don't return occupancies that are already expired
+    @Query("SELECT o FROM RoomOccupancy o WHERE o.room.id IN :roomIds AND o.endAt IS NULL AND o.expectedEndAt > :now")
+    List<RoomOccupancy> findActiveNonExpiredByRoomIds(@Param("roomIds") List<Long> roomIds, @Param("now") LocalDateTime now);
 }
