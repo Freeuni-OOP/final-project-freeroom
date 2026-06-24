172e1ba6a86eb195c8dc653b55d733c46cde4bf4
chap0 <nikolozchapidze4@gmail.com>
2026-06-24 22:40:12 +0400

    feat(friends backend): implemented friendship entity, added to the base

--- server/src/main/java/ge/freeroom/freeroom/entities/Friendship.java
+++ server/src/main/java/ge/freeroom/freeroom/entities/Friendship.java
@@ -1,4 +1,43 @@
 package ge.freeroom.freeroom.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(
+        name = "friendship",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user1_id", "user2_id"})
+        }
+)
 public class Friendship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user1_id", nullable = false)
+    private User user1; // Always the lexicographically smaller of the two user IDs
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user2_id", nullable = false)
+    private User user2;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
