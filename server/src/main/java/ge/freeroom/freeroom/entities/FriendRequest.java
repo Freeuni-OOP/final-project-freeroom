72d1ec2c2de3c714dd824002418a55e94a77e33e
chap0 <nikolozchapidze4@gmail.com>
2026-06-24 22:34:47 +0400

    feat(friends backend): implemented friend request entity, added to the base

--- server/src/main/java/ge/freeroom/freeroom/entities/FriendRequest.java
+++ server/src/main/java/ge/freeroom/freeroom/entities/FriendRequest.java
@@ -1,4 +1,51 @@
 package ge.freeroom.freeroom.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "friend_request")
 public class FriendRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FriendRequestStatus status = FriendRequestStatus.PENDING;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+
 }
