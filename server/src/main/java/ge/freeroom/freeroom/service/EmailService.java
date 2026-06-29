392aedd5167be765e3031091a7ebf74c7164f793
GiorgiZabakhidze <zabaxidzegivi@gmail.com>
2026-06-29 21:56:11 +0400

    add EmailService and implement in RoomAvailabilityService

---
+++ server/src/main/java/ge/freeroom/freeroom/service/EmailService.java
@@ -0,0 +1,38 @@
+package ge.freeroom.freeroom.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendReservationConfirmation(String toEmail, Integer roomNumber, LocalDateTime expectedEndAt) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(toEmail);
+        mail.setSubject("FreeRoom - ოთახი დაჯავშნილია");
+        mail.setText(
+                "ოთახი " + roomNumber + " წარმატებით დაიჯავშნა.\n" +
+                        "დაჯავშნა მოქმედია: " + expectedEndAt.toLocalTime() + "-მდე."
+        );
+        mailSender.send(mail);
+    }
+
+    public void sendExpiryWarning(String toEmail, Integer roomNumber) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("FreeRoom - დაჯავშნილი დრო იწურება");
+        message.setText(
+                "ოთახი " + roomNumber + "-ის ჯავშნის დასრულებამდე დარჩენილია 10 წუთი"
+        );
+        mailSender.send(message);
+    }
+}
