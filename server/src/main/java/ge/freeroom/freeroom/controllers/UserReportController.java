514a16b40694714fbd0afed54c833b5cafa3e704
GiorgiZabakhidze <zabaxidzegivi@gmail.com>
2026-07-01 19:02:31 +0400

    removed unused import

--- server/src/main/java/ge/freeroom/freeroom/controllers/UserReportController.java
+++ server/src/main/java/ge/freeroom/freeroom/controllers/UserReportController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 public class UserReportController {
