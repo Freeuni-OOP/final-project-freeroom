6e897eec3f3b69a5446e6ff76eeafc328ff61ab1
chap0 <nikolozchapidze4@gmail.com>
2026-07-06 01:12:41 +0400

    feat(friendship sockets): updated WebSocketConfig

--- server/src/main/java/ge/freeroom/freeroom/config/WebSocketConfig.java
+++ server/src/main/java/ge/freeroom/freeroom/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package ge.freeroom.freeroom.config;
 
+import ge.freeroom.freeroom.websocket.UserHandshakeHandler;
+import ge.freeroom.freeroom.websocket.WebSocketAuthInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +12,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final WebSocketAuthInterceptor webSocketAuthInterceptor;
+
+    public WebSocketConfig(WebSocketAuthInterceptor webSocketAuthInterceptor) {
+        this.webSocketAuthInterceptor = webSocketAuthInterceptor;
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -20,6 +29,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
+                .setHandshakeHandler(new UserHandshakeHandler())
                 .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(webSocketAuthInterceptor);
     }
 }
